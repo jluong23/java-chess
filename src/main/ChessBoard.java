f0f3224ef4234c7f06a2bff8e428568e1c5463c8
James Luong <jamesluong@hotmail.co.uk>
2020-03-27 23:30:49 +0000

    Setup chess subclasses templates

---
+++ src/main/ChessBoard.java
@@ -0,0 +1,18 @@
+package main;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessBoard extends Board {
+
+	public ChessBoard(Piece[][] board) {
+		super(board);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reset() {
+		
+	}
+
+}
---
+++ src/main/ChessPiece.java
@@ -0,0 +1,21 @@
+
+package main;
+
+import boardgame.Colour;
+import boardgame.Coordinate;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+
+	public ChessPiece(Colour colour) {
+		super(colour);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Coordinate[] getPossibleMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
